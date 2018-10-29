99a6f16002b316dbc42ca3a4cf0c7e1fa3bcdf92
ClayGale <clay.aj.gale@gmail.com>
2018-10-29 15:19:21 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"net/http"
+	_ "database/sql"
+	_ "html/template"
+	_ "net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	http.HandleFunc("/", mainHandler)
+
 }
