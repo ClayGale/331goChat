1733c42d5762b039b8cfd95d5fd68f7d29fc3b3c
ClayGale <clay.aj.gale@gmail.com>
2018-10-30 08:38:55 -0700

    removed error check?

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 }
 
 //d, _ := os.Getwd()
-var chatTemplate = template.Must(template.ParseFiles(chatTemplateHTML))
+var chatTemplate = template.ParseFiles(chatTemplateHTML)
 
 func login(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
